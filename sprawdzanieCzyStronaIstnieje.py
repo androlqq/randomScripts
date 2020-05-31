f06efc5eb4bf2d6635035efa59a39ede9787988b
androlqq <androlqq@gmail.com>
2020-05-31 22:11:45 +0200

    dupa

--- sprawdzanieCzyStronaIstnieje.py
+++ sprawdzanieCzyStronaIstnieje.py
@@ -1,13 +1,13 @@
 import requests
 
-dzialajaceStrony = open("dzialajacestrony.txt", 'w')
+dzialajaceStrony = open("dzialajacestrony.txt", 'r+')
 
 with open("listaStron.txt", 'r') as listaStron:
     for line in listaStron:
-        print(line)
-        zapytanieDoStrony = requests.get(line)
+        #print(line)
+        zapytanieDoStrony = requests.get(line.strip())
         if zapytanieDoStrony.status_code == 200:
             dzialajaceStrony.write(line)
+    print(dzialajaceStrony.read())
+    dzialajaceStrony.close()
 
-print(dzialajaceStrony.read())
-dzialajaceStrony.close()
